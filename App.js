71e93d12689e798352f7c2b3ad8b6a86bb12e79e
Swetha Reddy <swethakiranmv@gmail.com>
2023-03-31 12:28:54 +0530

    basic calculator

--- App.js
+++ App.js
@@ -2,23 +2,19 @@ function display(val){
 
     document.getElementById('result').value += val
 
-    return val;
-
 }
 function solve(){
 
     let x = document.getElementById('result').value
 
-    let y = eval(x);
+    let res = eval(x);
 
-    document.getElementById('result').value = y
-
-    return y
+    document.getElementById('result').value = res;
 
 }
 function clearAll(){
 
-    document.getElementById('result').value = ''
+    document.getElementById('result').value = '';
 
 }
 function deleteOne()
